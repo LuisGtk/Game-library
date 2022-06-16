4f08f3a7294168e7265b06a520852eb456e3d1d4
LuisGtk <luisgonzalez189@gmail.com>
2022-06-16 14:11:45 -0400

    installed browser router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 // import Gamelist from './components/gameList/gameList';
 import Data from './components/gameInfo/gameInfo';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>The Gaming Library</h1>
       </header>
       <div>
-        <Data />
+
 
       </div>
-
-
-
-
-
-
-
 
     </div>
   );
--- src/components/gameInfo/gameInfo.js
+++ src/components/gameInfo/gameInfo.js
@@ -1,54 +1,25 @@
-import axios from "axios"
-import React, { useEffect, useState } from "react"
+// import axios from "axios"
+import React, { useState } from "react";
 
 export default function Data() {
 
-    const [game, setGame] = useState()
+    const [gameData, setgameData] = useState();
+    const apiURL = ("https://www.freetogame.com/api/games");
+    //////////////////////////////////////////////////////////////////////////////////
 
-    useEffect(() => {
-        fetchData()
-    }, [])
+    // // logs data
+    fetch(apiURL)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        });
 
-    const fetchData = async () => {
-        const { data } = await axios.get("http://www.gamespot.com/api/games/?api_key=90e639df9ebfdb837daaf6c5cc836c3231412333")
-        setGame(data)
-    }
 
     return (
-        <div className="library">
-
-            {game.map(game => (
-
-                <div key={game.name}>
-                    <h1>{game.id}</h1>
-                </div>
-            ))}
-
-
-
-
-
+        <div>
+            <h1>whatever</h1>
+            <p>{gameData}</p>
         </div>
     )
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// const Data = (props) => {
-//     const [stateVariable, setStateVariable] = useState('');
-//     return (
-//         <div>
-//             <h1>whatever</h1>
-//         </div>
-//     )
-// }
