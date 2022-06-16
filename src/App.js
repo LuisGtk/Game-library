85c702390fab571edaeaf0ede3015eba4d979675
LuisGtk <luisgonzalez189@gmail.com>
2022-06-16 14:13:35 -0400

    creating home comp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { Routes , Route, Link } from 'react-router-dom';
 import './App.css';
 // import Gamelist from './components/gameList/gameList';
 import Data from './components/gameInfo/gameInfo';
@@ -10,10 +11,22 @@ function App() {
       <header className="App-header">
         <h1>The Gaming Library</h1>
       </header>
-      <div>
 
 
-      </div>
+
+
+
+
+
+
+
+
+
+      <main>
+  <Routes>
+  <Route path='/Games' element={<Data/>} />
+  </Routes>
+</main>
 
     </div>
   );
