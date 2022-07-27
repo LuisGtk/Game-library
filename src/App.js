515472b8056361723c3cac18c13cd012c10fc74f
LuisGtk <luisgonzalez189@gmail.com>
2022-07-27 14:11:41 -0400

    succesfully created link that directs user to site for any game clicked //didnt save code for some reason

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home/Home';
-import GameInfo from './components/GameInfo/GameInfo';
-import Favorites from './components/Favorites/Favorites';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home";
+import GameInfo from "./components/GameInfo/GameInfo";
+import Favorites from "./components/Favorites/Favorites";
 
 function App() {
-
   ///////////////////////////////////////////////////////////////
   return (
     <div className="App">
       <header className="App-header">
-<Link className='homeBtn' to = '/'>
-        <h1>React-Game Collection</h1>
- </Link>
-        <div className='flex'>
-          <Link to='/Favorites'>
-            <button className='collection'>Favorites</button>
+        <Link className="homeBtn" to="/">
+          <h1>React-Game Collection</h1>
+        </Link>
+        <div className="flex">
+          <Link to="/Favorites">
+            <button className="collection">
+              Favorites
+            </button>
           </Link>
         </div>
       </header>
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/GameInfo' element={<GameInfo />} />
-          <Route path='/Favorites' element={<Favorites />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/GameInfo" element={<GameInfo />} />
+          <Route
+            path="/Favorites" element={<Favorites />}
+          />
         </Routes>
       </main>
     </div>
--- src/components/gameInfo/gameInfo.js
+++ src/components/gameInfo/gameInfo.js
@@ -1,14 +1,9 @@
-
 import React, { useState } from "react";
 
 export default function GameInfo() {
-
-
-
-    return (
-        <div>
-            <h1>hello from the game info</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>hello from the game info</h1>
+    </div>
+  );
 }
-
